7c74e8a19fc6673dce3b7b3e9072bf80ba5bfd39
Tidarat-An <s6403052422214@kmutnb.ac.th>
2022-09-01 14:43:49 +0700

    post.php

---
+++ post.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>post</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Meaow Meaow</h1>
+    <hr>
+    <div align="center" >
+        ต้องการดูกระทู้หมายเลข <?php echo $_GET["id"];?>
+        </div><br>
+        <table style="border: 2px solid black; width:40%;" align="center">
+        <tr><td colspan="2" style="background-color:#6CD2FE;">แสดงความคิดเห็น</td></tr>
+        <tr><td><textarea name="" id="" cols="" rows=""></textarea></td></tr>
+        <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+    </table><br>
+    <center><a href="index.php">กลับไปหน้าหลัก</center></a>
+</body>
+</html>
